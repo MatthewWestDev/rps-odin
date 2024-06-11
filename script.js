9988f68fb7f872ae42a07291fd13cfb2b6f3d1a1
Matthew West <128643572+MatthewWestDev@users.noreply.github.com>
2024-06-11 14:53:19 -0500

    Complete game play

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
     let computerScore = 0;
     let humanScore = 0;
     let roundCounter = 0;
+    const round = document.querySelector(".round");
+    const rounds = document.querySelector(".rounds");
     const rockButton = document.querySelector(".rock");
     const paperButton = document.querySelector(".paper");
     const scissorsButton = document.querySelector(".scissors");
-
-   
 
     rockButton.addEventListener("click", () => {
         humanChoice = "Rock";
@@ -33,26 +33,62 @@
 function playRound() {
 
         const computerChoice = getComputerChoice();
+
+        const score = document.querySelector(".score");
+        const result = document.createElement("p");
            
         if (humanChoice == computerChoice) {
-            console.log("Draw! " + humanChoice + " vs. " + computerChoice);
+            result.textContent = "Tie! " + humanChoice + " vs. " + computerChoice;
+            rounds.prepend(result);
         } else if (humanChoice == "Rock" && computerChoice == "Paper" || humanChoice == "Paper" && computerChoice == "Scissors" || humanChoice == "Scissors" && computerChoice == "Rock") {
-            console.log("Point for Computer! " + "You chose: " + humanChoice + " vs. " + "Computer chose: " + computerChoice);
+            result.textContent = "Point for JS! " + humanChoice + " vs. " + computerChoice;
+            rounds.prepend(result);
             computerScore++;  // score increment
         } else if (humanChoice == "Rock" && computerChoice == "Scissors" || humanChoice == "Paper" && computerChoice == "Rock" || humanChoice == "Scissors" && computerChoice == "Paper") {
-            console.log("Point for You! " + "You chose: " + humanChoice + " vs. " + "Computer chose: " + computerChoice);
+            result.textContent = "Point for You! " + humanChoice + " vs. " + computerChoice;
+            rounds.prepend(result);
             humanScore++; // score increment
         } 
         roundCounter++;  // increment round counter
-        console.log("Round: " + roundCounter + " You: " + humanScore + " Computer: " + computerScore);
+        round.textContent = "Round: " + roundCounter;
+        
+        score.textContent = "You: " + humanScore + " vs. " + " JS: " + computerScore;
+
 
         if (computerScore === 5) {
-                console.log("You Lose!");
-            } else if (humanScore === 5) {
-                console.log("You Win!");
-            } else {
-                console.log("Draw!");
-            }
+            score.textContent = "YOU LOSE!";
+            score.classList.add("loser");
+            setGameOver();
+
+        } else if (humanScore === 5) {
+            score.textContent = "YOU WIN!";
+            score.classList.add("winner");
+            setGameOver();
+            } 
         
+        function setGameOver() {
+            rockButton.disabled = true;
+            paperButton.disabled = true;
+            scissorsButton.disabled = true;
+            resetButton = document.createElement("button");
+            resetButton.textContent = "Play Again...";
+            rounds.prepend(resetButton);
+            resetButton.addEventListener("click", resetGame);
+        }
+
+        function resetGame() {
+            score.textContent = "Click a button to play...";
+            rockButton.disabled = false;
+            paperButton.disabled = false;
+            scissorsButton.disabled = false;
+            humanScore = 0;
+            computerScore = 0;
+            roundCounter = 1;
+            round.textContent = "Round: " + roundCounter;
+            score.textContent = "You: 0 vs. JS: 0";
+            rounds.textContent = "";
+            
+        }
+
 }
 
